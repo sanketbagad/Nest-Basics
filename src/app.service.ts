f3cc325714717cc01cd69ac459883de239d5460a
Sanket Bagad <alencolins@gmail.com>
2022-01-11 18:07:46 +0530

    Just fun purposes�

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'API created by Sanket Bagad As a part of the NESTJS course';
   }
 }
